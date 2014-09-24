e6c82b9b2952fbc4806a76d40ed45e8a02a4f71a
Mike Kauffman <mkauffman@quickleft.com>
2014-09-24 16:03:31 -0600

    editing an athlete in real time on show page

--- app/scripts/controllers/athleteShow.js
+++ app/scripts/controllers/athleteShow.js
@@ -1,15 +1,28 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('athleteApp')
-    .controller('AthleteShowCtrl', function($scope, $routeParams, AthleteService, ActivityService) {
-      AthleteService.get($routeParams.athleteId).then(function(athleteResponse) {
+    .controller('AthleteShowCtrl', function ($http, $scope, $routeParams, AthleteService, ActivityService) {
+      AthleteService.get($routeParams.athleteId).then(function (athleteResponse) {
         $scope.athlete = athleteResponse.data;
       });
 
-      ActivityService.getList({athlete_id: $routeParams.athleteId}).then(function(activitiesResponse) {
+      ActivityService.getList({athlete_id: $routeParams.athleteId}).then(function (activitiesResponse) {
         $scope.activities = activitiesResponse.data;
       });
+
+      $scope.editAthleteName = function () {
+        $scope.editingAthleteName = true;
+      };
+
+      $scope.finishEditing = function (athlete) {
+        $http
+          .put(
+            'http://localhost:3000/athletes/' + $routeParams.athleteId, athlete
+        ).then(function () {
+            $scope.editingAthleteName = false;
+          });
+      };
     });
 })();
